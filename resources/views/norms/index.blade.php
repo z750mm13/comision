58ca91eef4823e31c068ab8d4f468b80c621101b
Mauricio <maurir_@hotmail.com>
2020-09-04 01:28:01 -0500

    Ajuste responsive norms.index

--- resources/views/norms/index.blade.php
+++ resources/views/norms/index.blade.php
@@ -22,9 +22,10 @@
   <div class="card mb-3"> <!-- Borde primario primary danger warning-->
     <div class="card-header">{{$norm->codigo}}</div>
     <div class="card-body"> <!-- Texto primario -->
-      <h6 class="card-subtitle mb-1 text-muted">Descripcion de la norma</h6>
-      <p class="card-text">{{ substr($norm->titulo, 0, 35)."..." }}</p>
-      <div class="progress-wrapper">
+      <div class="col">
+        <h6 class="card-subtitle mb-1 text-muted">Descripcion de la norma</h6>
+        <p class="card-text">{{ substr($norm->titulo, 0, 35)."..." }}</p>
+        <div class="progress-wrapper">
         <div class="progress-info">
           <div class="progress-label">
             <span>Asignación</span>
@@ -36,8 +37,9 @@
         <div class="progress">
           <div class="progress-bar bg-gradient-success" role="progressbar" aria-valuenow="{{$norm->asignado}}" aria-valuemin="0" aria-valuemax="100" style="width: {{$norm->asignado}}%;"></div>
         </div>
+        </div>
+        <a class="stretched-link" href="/norms/{{$norm->id}}" class="card-link">Ver mas...</a>
       </div>
-      <a class="stretched-link" href="/norms/{{$norm->id}}" class="card-link">Ver mas...</a>
     </div>
   </div>
   </div>
